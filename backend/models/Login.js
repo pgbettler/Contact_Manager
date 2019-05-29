2056f7475cf7a43130c7cac2117c1ac9c1e59667
alex1bu <51127491+alex1bu@users.noreply.github.com>
2019-05-29 02:02:35 -0400

    Add files via upload

---
+++ backend/models/Login.js
@@ -0,0 +1,24 @@
+// This is the data model (Schema) for a login information.
+// By using this code, we’re creating a new Mongoose model for the login entity
+// with the following properties included:
+// 1: username
+// 2: password
+
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+let Login = new Schema({
+    username:
+    {
+        type: String
+    },
+    password:
+    {
+        type: String
+    }
+});
+
+// The model is created by defining a new Schema first and then creating 
+// the model by using the mongoose.model method.
+export default mongoose.model('Login', Login);
